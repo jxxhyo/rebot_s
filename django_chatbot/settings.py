61135fc57ac1236b2cbda1772e6b1b453f61c0a6
jxxhyo <122712519+jxxhyo@users.noreply.github.com>
2024-05-19 11:48:50 +0900

    change12

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -55,6 +55,23 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-requested-with',
+    'x-csrftoken'
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
 
 ROOT_URLCONF = "django_chatbot.urls"
 
