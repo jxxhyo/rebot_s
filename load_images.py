e02d434346d676602f90380da78e32f1e31ce7ad
jxxhyo <122712519+jxxhyo@users.noreply.github.com>
2024-06-08 00:03:25 +0900

    plusimage1

--- load_images.py
+++ load_images.py
@@ -4,13 +4,13 @@ from django.core.files import File
 
 # Django 프로젝트 설정 파일 경로
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chatbot.settings')  # 'my_django_project'를 실제 프로젝트 이름으로 변경
-
 django.setup()
 
 from chatbot.models import ResImage, Restaurant  # 'my_app'을 실제 앱 이름으로 변경
+ResImage.objects.all().delete()
 
 # 파일들이 저장된 루트 디렉토리 설정
-root_dir = 'dall-image'
+root_dir = 'dalle-image'
 
 # 디렉토리 트리 순회
 for restaurant_name in os.listdir(root_dir):
@@ -18,6 +18,9 @@ for restaurant_name in os.listdir(root_dir):
     if os.path.isdir(restaurant_path):
         # 레스토랑 객체 가져오기 (없는 경우 생성)
         restaurant, created = Restaurant.objects.get_or_create(name=restaurant_name)
+        
+        # ResImage 객체 가져오기 (없는 경우 생성)
+        res_image, created = ResImage.objects.get_or_create(restaurant=restaurant, image_name=restaurant_name)
 
         # 이미지 파일들 순회
         for image_type in ['chi_image', 'eng_image', 'jap_image', 'kor_image']:
@@ -26,10 +29,6 @@ for restaurant_name in os.listdir(root_dir):
                 for image_file in os.listdir(image_path):
                     if image_file.endswith('.jpg'):  # 확장자는 필요에 따라 조정
                         file_path = os.path.join(image_path, image_file)
-                        image_name = image_file.split('_gen_new_image_')[0]  # 파일명에서 이미지 이름 추출
-
-                        # ResImage 객체 생성 및 저장
-                        res_image = ResImage(restaurant=restaurant, image_name=image_name)
 
                         # 이미지 필드에 파일 추가
                         with open(file_path, 'rb') as f:
@@ -42,5 +41,5 @@ for restaurant_name in os.listdir(root_dir):
                             elif image_type == 'kor_image':
                                 res_image.image_ko.save(image_file, File(f), save=False)
 
-                        # 객체 저장
-                        res_image.save()
+        # 객체 저장
+        res_image.save()
